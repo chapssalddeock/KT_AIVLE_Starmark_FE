72bb1a06949c1347312fbbac36d05104bfea09f3
ChanGyu <abcd@gmail.com>
2023-06-13 17:00:32 +0900

    ListView

--- src/components/tempbody/Body.js
+++ src/components/tempbody/Body.js
@@ -1,26 +1,81 @@
 
-import { useState } from 'react';
+
+import { useState, useEffect, useRef  } from 'react';
+import styled from 'styled-components';
+
 export default function TempBody() {
     const [searchQuery, setSearchQuery] = useState('');
     const [searchHistory, setSearchHistory] = useState([]);
+    const [searchResults, setSearchResults] = useState([]);
+    const [showSuggestions, setShowSuggestions] = useState(false);
     const MAX_HISTORY_LENGTH = 5;
+    const suggestedItemsRef = useRef(null);
+    const [filteredItems, setFilteredItems] = useState([]);
+
+
     const handleSearch = () => {
         if (searchQuery.trim() === '') {
             return; // 검색어가 비어있으면 동작하지 않음
-          }
-    // 검색 로직 구현
+        }
+        // 검색 로직 구현
         console.log('검색 실행:', searchQuery);
         const updatedHistory = [searchQuery, ...searchHistory.slice(0, MAX_HISTORY_LENGTH - 1)];
         setSearchHistory(updatedHistory);
         setSearchQuery('');
+        setShowSuggestions(false);
     };
+    const handleSuggestedItemClick = (value) => {
+        setSearchQuery(value);
+        searchInputRef.current.focus(); // useRef를 사용하여 DOM 요소에 접근하여 포커스 설정
+    };
+
+    useEffect(() => {
+        const items = ['abc', 'def', 'fqw', 'vxcv', 'bgf', 'dfg'];
+        const filteredItems  = items.filter((item) => item.toLowerCase().includes(searchQuery.toLowerCase()));
+        setFilteredItems(filteredItems);
+        // setShowSuggestions(filteredItems.length > 0);
+        // const suggestedItemsElement = suggestedItemsRef.current; // ref로 요소 참조
+        // suggestedItemsElement.innerHTML = '';
+
+        /*const suggestedItems = items
+            .filter((item) => item.startsWith(searchQuery))
+            .map((item, index) => (
+                <div key={index} className="item" onClick={() => handleSuggestedItemClick(item)}>
+                  {item}
+                </div>
+            )); */
+        
+            // 추천 아이템 업데이트
+        //setSuggestedItems(suggestedItems);
+        // filteredItems.forEach((item) => {
+        //     const suggestedItem = document.createElement('div');
+        //     suggestedItem.className = 'item';
+        //     suggestedItem.textContent = item;
+        //     suggestedItem.addEventListener('click', () => handleSearchHistory(item));
+        //     suggestedItemsElement.appendChild(suggestedItem);
+        // });
+        
+    }, [searchQuery]);
+
+        
+    
+    const handleSearchInput = (e) => {
+        const input = e.target.value;
+        setSearchQuery(input);
+        setShowSuggestions(input !== '');
+    };
+    const handleSearchHistory = (query) => {
+        setSearchQuery(query);
+        setShowSuggestions(false); // 항목 선택 후 가시성 해제
+    };
+    
 
     return (
         <div>
             <div className="sidebar">
 
                 <div class="search-container">
-                    <input class="search-input" type="text" placeholder="Search" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />                                        
+                    <input class="search-input" type="text" placeholder="Search" value={searchQuery} onChange={handleSearchInput} />                                        
                     <input class="search-button" type="image" src="/public/img/search_RG.jpg" alt = 'ss' onClick={handleSearch} />                                          
                 </div>
                 <div class="search-history">
@@ -28,6 +83,27 @@ export default function TempBody() {
                     <button key={index} class="search-record">{query}</button>
                 ))}
                 </div>
+                {showSuggestions && (
+                    <div id="suggestion_box">
+                        {filteredItems.map((item, index) => (
+                            <div
+                                key={index}
+                                className="suggested-item"
+                                onClick={() => handleSuggestedItemClick(item)}
+                            >
+                                {item}
+                            </div>
+                        ))}
+                        {/* <div id="suggested_items" ref={suggestedItemsRef}>
+                            {filteredItems.map((item, index) => (
+                                <div key={index} className="suggested-item" onClick={() => handleSuggestedItemClick(item)}>
+                                    {item}
+                                </div>
+                            ))}
+                        </div>  */}
+                    </div>
+                )}
+                
                         
 
         
@@ -42,4 +118,5 @@ export default function TempBody() {
             </div>
         </div>
     );
+    
 }
