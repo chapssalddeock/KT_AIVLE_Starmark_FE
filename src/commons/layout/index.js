79566f66d1e9e9a182ace342f504f5ebf5f7537c
chapssalddeock <kchae10824@gmail.com>
2023-06-14 08:25:53 +0900

    Content Area Layout

--- src/commons/layout/index.js
+++ src/commons/layout/index.js
@@ -3,11 +3,9 @@ import { useRouter } from "next/router"
 import NavBar from "../../components/NavBar/NavBar.js"
 import NavBar2 from "../../components/NavBar/NavBar2.js"
 
-
-import Footer from "../../components/footer/Footer"
-import TempBody from "../../components/tempbody/Body"
 // 레이아웃 속성 (즉, head body navBar, footer 등등을 여기에 끌어와서 정의)
 
+import ContentArea from "../../components/ContentArea/ContentArea"
 
 
 // 어느 페이지에서 없애는 조건부 렌더링을 할 건지 경로 정의
@@ -25,8 +23,7 @@ export default function Layout(props) {
             {!isHidden && <NavBar />}
             {isHidden && <NavBar2 />}
             <div>{props.children}</div>
-            <TempBody />
-            <Footer />
+            <ContentArea />
         </>
     )
 }
