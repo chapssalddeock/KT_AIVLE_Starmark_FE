e5cd309f75f97a67dffbba3b89b487ef344465dd
chapssalddeock <kchae10824@gmail.com>
2023-06-15 13:56:03 +0900

    Modify NavBar Components

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -3,6 +3,9 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+
+
+
 export default function NavBar() {
     const router = useRouter();
 
