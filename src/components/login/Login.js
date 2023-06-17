b09dfca0516f439041c9a7ba2b904fec22e131ff
sora0319 <sojeoung536985@gmail.com>
2023-06-17 21:36:14 +0900

    fix : login from email

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,5 +1,5 @@
-import { CustomerPage, TitleSpace, LoginTitle, ButtonDesign, InputSpace, TopScroll, ClickToTop } 
-from "../../../styles/Login_Emotion"
+import { CustomerPage, TitleSpace, LoginTitle, ButtonDesign, InputSpace, TopScroll, ClickToTop }
+  from "../../../styles/Login_Emotion"
 import { Checkbox, Form, Input } from 'antd';
 
 const onFinish = (values) => {
@@ -10,8 +10,8 @@ const onFinishFailed = (errorInfo) => {
 };
 
 
-const LoginPage = ({ scrollToTop }) =>  {
-    return (
+const LoginPage = ({ scrollToTop }) => {
+  return (
     <CustomerPage>
       <TitleSpace>
         <LoginTitle>
@@ -19,72 +19,65 @@ const LoginPage = ({ scrollToTop }) =>  {
         </LoginTitle>
       </TitleSpace>
       <InputSpace>
-      <Form
-        name="basic"
-        labelCol={{span: 7, offset: 0}}
-        wrapperCol={{span: 16,}}
-        style={{ maxWidth: 600,}}
-        initialValues={{remember: true,}}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your username!',
-            },
-          ]}
+        <Form
+          name="basic"
+          labelCol={{ span: 7, offset: 0 }}
+          wrapperCol={{ span: 16, }}
+          style={{ maxWidth: 600, }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
         >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your password!',
-            },
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item
-          name="remember"
-          valuePropName="checked"
-          wrapperCol={{
-            offset: 8,
-            span: 16,
-          }}
-        >
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-        <Form.Item
-          wrapperCol={{
-            offset: 3,
-            span: 20,
-          }}
-        >
-        <ButtonDesign type="primary" htmlType="submit" className="login-form-button">
-          Log in
-        </ButtonDesign>
-        Or <a href="">register now!</a>
-        </Form.Item>
-      </Form>
+          <Form.Item
+            name="email"
+            label="E-mail"
+            rules={[
+              {
+                type: 'email',
+                message: 'The input is not valid E-mail!',
+              },
+              {
+                required: true,
+                message: 'Please input your E-mail!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            wrapperCol={{
+              offset: 3,
+              span: 20,
+            }}
+          >
+            <ButtonDesign type="primary" htmlType="submit" className="login-form-button">
+              Log in
+            </ButtonDesign>
+            Or <a href="">register now!</a>
+          </Form.Item>
+        </Form>
       </InputSpace>
       <TopScroll>
         <ClickToTop onClick={scrollToTop}>맨 위로 이동</ClickToTop>
       </TopScroll>
 
-      
-    </CustomerPage>
-    );
 
-    
+    </CustomerPage>
+  );
+
+
 };
 
 export default LoginPage;
