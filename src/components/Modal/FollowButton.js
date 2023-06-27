3c5256acd77762092f7e5d2760631d4458df4cbc
sora0319 <sojeoung536985@gmail.com>
2023-06-27 15:41:25 +0900

    feat: add axios post communication in FollowButton

--- src/components/Modal/FollowButton.js
+++ src/components/Modal/FollowButton.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Button } from 'antd';
+import usePOST from '../../axios/POST';
+import axios from 'axios';
 
 export default function FollowButton({ user_isFollowing, user_id }) {
     const [isFollowing, setIsFollowing] = useState([]);
+    const { fetchData : postFetchData, data: postData, error: postError } = usePOST();
 
     useEffect(() => {
         const fetchFollowStatus = async () => {
@@ -13,27 +15,18 @@ export default function FollowButton({ user_isFollowing, user_id }) {
     }, [user_isFollowing]);
 
     const handleFollow = async (user_id) => {
-        try {
-            const config = {
-                headers: {
-                    Authorization:
-                        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjg4NTQyMzU4LCJpYXQiOjE2ODcyNDYzNTgsImp0aSI6ImI2YTU0OWJkOWQxYTQzMWFhNDE3NmFmMmFmMjVjYjQ2IiwidXNlcl9pZCI6MiwidXNlcm5hbWUiOiJcdWQxNGNcdWMyYTRcdWQyYjgwMyJ9.cTZokEPKCxNTo6S-BXdv2pRakGRlnIBqzWAGHQKI6Nk',
-                    'Content-Type': 'application/json',
-                },
-            };
-
-            // 팔로우 API 호출
-            const response = await axios.post(
-                'http://kt-aivle.iptime.org:40170/api/follows/',
-                { user_id: user_id },
-                config
-            );
-            console.log(response.data.message);
-            setIsFollowing(true);
-        } catch (error) {
-            console.error(error);
-        }
+        console.log(user_id);
+        await postFetchData('/follows/', { user_id:  user_id});
     };
+
+    useEffect(() => {
+        if (postData) {
+            console.log(postData);
+            setIsFollowing(true);
+        } else if (postError) {
+            console.error(postError);
+        }
+    }, [postData, postError]);
 
     const handleUnfollow = async (user_id) => {
         try {
@@ -66,4 +59,69 @@ export default function FollowButton({ user_isFollowing, user_id }) {
 
 
 
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import { Button } from 'antd';
 
+// export default function FollowButton({ user_isFollowing, user_id }) {
+//     const [isFollowing, setIsFollowing] = useState([]);
+
+//     useEffect(() => {
+//         const fetchFollowStatus = async () => {
+//             setIsFollowing(user_isFollowing);
+//         };
+//         fetchFollowStatus();
+//     }, [user_isFollowing]);
+
+//     const handleFollow = async (user_id) => {
+//         try {
+//             const config = {
+//                 headers: {
+//                     Authorization:
+//                         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjg4NTQyMzU4LCJpYXQiOjE2ODcyNDYzNTgsImp0aSI6ImI2YTU0OWJkOWQxYTQzMWFhNDE3NmFmMmFmMjVjYjQ2IiwidXNlcl9pZCI6MiwidXNlcm5hbWUiOiJcdWQxNGNcdWMyYTRcdWQyYjgwMyJ9.cTZokEPKCxNTo6S-BXdv2pRakGRlnIBqzWAGHQKI6Nk',
+//                     'Content-Type': 'application/json',
+//                 },
+//             };
+
+//             // 팔로우 API 호출
+//             const response = await axios.post(
+//                 'http://kt-aivle.iptime.org:40170/api/follows/',
+//                 { user_id: user_id },
+//                 config
+//             );
+//             console.log(response);
+//             console.log(response.data.message);
+//             setIsFollowing(true);
+//         } catch (error) {
+//             console.error(error);
+//         }
+//     };
+
+//     const handleUnfollow = async (user_id) => {
+//         try {
+//             const config = {
+//                 headers: {
+//                     Authorization:
+//                         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjg4NTQyMzU4LCJpYXQiOjE2ODcyNDYzNTgsImp0aSI6ImI2YTU0OWJkOWQxYTQzMWFhNDE3NmFmMmFmMjVjYjQ2IiwidXNlcl9pZCI6MiwidXNlcm5hbWUiOiJcdWQxNGNcdWMyYTRcdWQyYjgwMyJ9.cTZokEPKCxNTo6S-BXdv2pRakGRlnIBqzWAGHQKI6Nk',
+//                     'Content-Type': 'application/json',
+//                 },
+//             };
+
+//             // 언팔로우 API 호출
+//             const response = await axios.delete(
+//                 'http://kt-aivle.iptime.org:40170/api/follows/',
+//                 { ...config, data: { user_id: user_id } }
+//             );
+//             console.log(response.data.message);
+//             setIsFollowing(false);
+//         } catch (error) {
+//             console.error(error);
+//         }
+//     };
+
+//     return (
+//         <Button onClick={isFollowing ? () => handleUnfollow(user_id) : () => handleFollow(user_id)}>
+//             {isFollowing ? 'Following' : 'Follow'}
+//         </Button>
+//     );
+// };
