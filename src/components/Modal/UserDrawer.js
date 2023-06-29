0f508d8d61976c0f673e5d0907ac95e1e4c9600d
ChanGyu <abcd@gmail.com>
2023-06-29 17:08:53 +0900

    Drawer Modify

--- src/components/Modal/UserDrawer.js
+++ src/components/Modal/UserDrawer.js
@@ -1,11 +1,38 @@
 import React, { useState } from 'react';
-import { Drawer, Avatar } from 'antd';
+import { Drawer, Input, Avatar , Button, Modal, Form, Switch , Tooltip } from 'antd';
 import FollowButton from '../Modal/FollowButton';
-
+import usePOST from '../../AuthCommunicate/POST';
 
 
 // id, email, username, profile_image, is_following, following_cnt, follower_cnt, bookmark_cnt 넘어옴
-export default function UserDrawer({ isOpen, onClose, userProfile }) {
+export default function UserDrawer({ isOpen, onClose, userProfile, urls }) {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedUrl, setSelectedUrl] = useState('');
+  const { fetchData: postFetchData, data: postData, error: postError } = usePOST();
+  const [form] = Form.useForm();
+  const handleAddButtonClick = (url) => {
+    setSelectedUrl(url);
+    setModalVisible(true);
+  };
+
+  const handleModalClose = () => {
+    setSelectedUrl('');
+    setModalVisible(false);
+  };
+  const handleSubmit = () => {
+    form.validateFields().then((values) => {
+      const formData = {
+        type: 'string',
+        title: values.title,
+        url: selectedUrl,
+        is_public: values.is_public,
+      };
+      // 이후 formData를 사용하여 데이터 처리하는 로직을 작성합니다.
+      console.log(formData);
+      form.resetFields();
+      handleModalClose();
+    });
+  };
 
 
   return (
@@ -32,6 +59,15 @@ export default function UserDrawer({ isOpen, onClose, userProfile }) {
               <div>
                 <FollowButton user_isFollowing={userProfile.is_following} user_id={userProfile.id} />
               </div>
+              <strong>Url:</strong>
+              {urls.map((url, index) => (
+                <div key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '20px' }}>
+                  <div style={{ flex: 1, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{url}</div>
+                  <Button onClick={() => handleAddButtonClick(url)} type="primary" size="small">
+                    Add
+                  </Button>
+                </div>
+              ))}
             </div>
             {/* 추가적인 유저 정보를 표시하는 코드를 작성하세요 */}
           </div>
@@ -41,6 +77,36 @@ export default function UserDrawer({ isOpen, onClose, userProfile }) {
           <p className="site-description-item-profile-p">유저 정보를 불러오는 중입니다...</p>
         )}
       </Drawer>
+      <Modal
+        visible={modalVisible}
+        onCancel={handleModalClose}
+        onOk={handleSubmit}
+        title="Add Data"
+        
+      >
+        <Form form={form} layout="vertical">
+          <Form.Item
+            name="title"
+            label="Title"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter a title',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item name="url" label="URL">
+            <div style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+              <Input value={selectedUrl} disabled addonAfter={selectedUrl} />
+            </div>
+          </Form.Item>
+          <Form.Item name="is_public" label="Is Public" valuePropName="checked">
+            <Switch />
+          </Form.Item>
+        </Form>
+      </Modal>
     </>
   );
 };
