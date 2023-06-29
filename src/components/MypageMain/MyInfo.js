c7ff4335d411a2e471b2c0ea63366edc9b0a2d19
chapssalddeock <kchae10824@gmail.com>
2023-06-29 17:51:11 +0900

    feat: my info layout2

--- src/components/MypageMain/MyInfo.js
+++ src/components/MypageMain/MyInfo.js
@@ -1,18 +1,24 @@
 // 필요한 기능
 // 1. 현재 자기가 가진 프사, 닉네임, 이메일, 팔로워수, 북마크수 등 표시
 // 2. 이미지랑 닉네임은 수정이 가능하도록 서버와 통신
-import { MainFrame, ImgFrame, TitleFrame, ContentFrame } from '../../../styles/MyPage_Emotion';
+import { useState, useEffect } from 'react';
+import { MainFrame, ImgFrame, TitleFrame, ContentFrame, ImgChangeButton } from '../../../styles/MyPage_Emotion';
 import useGET from '../../AuthCommunicate/GET';
 
 export default function MyInfo() {
-    
+
     const [data, setData] = useState([]);
     const { fetchData: getFetchData, data: getData, error: getError } = useGET();
 
-
     const fetchData = async () => {
-        const config = {};
-        await getFetchData('/profile_img/', config);
+        //const config = {};
+        //토큰은 자동이라 get에선 config가 필요없음
+        const config = {
+            params: {
+                user_id: 0,
+            },
+        };
+        await getFetchData('/userinfo/', config);
     };
 
     useEffect(() => {
@@ -24,16 +30,23 @@ export default function MyInfo() {
     }, [getData, getError]);
 
     useEffect(() => {
-        fetchData(searchResult);
-    }, [currentPage, searchResult]);
+        fetchData();
+    }, []);
+
+    console.log("확인용", data);
 
 
     return (
 
         <>
             <MainFrame>
-                <TitleFrame></TitleFrame>
-                <ImgFrame></ImgFrame>
+                <TitleFrame>
+                    My Infomation
+                </TitleFrame>
+                <ImgFrame>
+                    <img src={data.profile_image} style={{ width: 300, height: 300 }}></img>
+                </ImgFrame>
+                <ImgChangeButton></ImgChangeButton>
                 <ContentFrame></ContentFrame>
             </MainFrame>
 
--- styles/MyPage_Emotion.js
+++ styles/MyPage_Emotion.js
@@ -1,65 +1,63 @@
 import styled from '@emotion/styled';
 
 
-// export const MainFrame = styled.div`
-//   height: calc(100vh - 100px); /* 뷰포트의 높이에서 NavBar의 높이(76px)를 뺀 값으로 높이 설정 */
-//   /* background-color: #5eacf2; */
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   border: solid red;
-// `
-
-// export const ImgFrame = styled.div`
-
-//   border : solid blue;
-//   position : relative;
-//   width :  300px;
-//   height : 300px;
-//   left : 50vh; 
-//   bottom: 10vh;
-
-// `
-
-// export const TitleFrame = styled.div`
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   border: solid;
-
-
-// `
-
-
-
 export const MainFrame = styled.div`
   width: 100%;
   height: 100%;
-  border: solid;
+  /* border: solid; */
   position : relative;
-
 `
 export const ImgFrame = styled.div`
   width :  300px;
   height : 300px;
   border: solid red;
   position : sticky;
-  left: 70%;
+  left: 75%;
   bottom: 30%;
 
 `
+
+
+export const ImgChangeButton = styled.button`
+  width :  300px;
+  height: 50px;
+  position : sticky;
+  left: 75%;
+  bottom: 15%;
+  border: solid blue;
+
+  display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  
+`
+
+
 export const TitleFrame = styled.div`
-  width: 70%;
+  width: 60%;
   height: 20%;
   position : sticky;
   bottom: 30%;
-  border: solid blue;
+  left: 25%;
+  //border: solid blue;
+
+  display: flex;
+  align-items: flex-end;
+  text-align: left;
+
+  //justify-content: center;
+  font-weight : bold;
+  font-size: 28px;
 `
+
+
 
 export const ContentFrame = styled.div`
   width: 60%;
   height: 60%;
   position : sticky;
   bottom: 20%;
+  left: 25%;
   border: solid yellow;
 `
