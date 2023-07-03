47b49b5e5c4bba9058517145f7459545aab8358f
chapssalddeock <kchae10824@gmail.com>
2023-07-03 15:59:53 +0900

    fix: dashboard options

--- src/components/ContentArea/MpContentArea.js
+++ src/components/ContentArea/MpContentArea.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import MpSideBar from '../MyPageSideBar/MpSideBar';
-import MpMain from '../MypageMain/MpMain';
-
-import { Layout } from 'antd';
-const { Content, Sider } = Layout;
 import MyInfo from '../MypageMain/MyInfo'
 import MyFollows from '../MypageMain/MyFollows';
 import MyDashBoard from '../MypageMain/MyDashBoard';
+
+import { Layout } from 'antd';
+const { Content, Sider } = Layout;
+
 
 
 export default function MpContentArea() {
@@ -35,30 +35,3 @@ export default function MpContentArea() {
     );
 };
 
-
-{/* <div style={{ marginTop: '-125px', width: '60%', height: '60%' }}>
-                        {selectedItem === 'sub1' && (
-                            <img src='img/UserProfile.png' style={{ width: '100%', height: '100%' }} />
-                        )}
-                        {selectedItem === 'sub3' && (
-                            <img src='img/PassWordModify.png' style={{ width: '100%', height: '100%' }} />
-                        )}
-                    </div>
-                    <Content style={{ padding: 24, marginTop: '-150px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                        {selectedItem === 'sub1' && (
-
-                            <div className='Box_user' style={{ border: '10px outset skyblue', borderRadius: '15px', width: '70%', height: '90%', overflow: 'hidden' }}>
-                                <MpMain selectedItem={selectedItem} />
-                            </div>
-                        )}
-
-                        {selectedItem === 'sub3' && (
-                            <div className='Box_user' style={{ border: '10px outset skyblue', borderRadius: '15px', width: '70%', height: '90%' }}>
-                                <MpMain selectedItem={selectedItem} />
-                            </div>
-                        )}
-
-                        {selectedItem !== 'sub1' && selectedItem !== 'sub3' && (
-                            <MpMain selectedItem={selectedItem} />
-                        )}
-                    </Content> */}
